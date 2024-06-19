2c6ffd3e281d766c428547812d4a2f44779961fe
Daniel Silva <danielsilvatsi@outlook.com>
2024-06-18 22:58:57 -0300

    git init

---
+++ Controllers/BancoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegracaoBrasilAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1[controller]")]
+    public class BancoController : ControllerBase
+    {
+       
+    }
+}
